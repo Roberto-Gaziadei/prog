8946bb5a4505f2dc1cf54818f8fab1d17bac7920
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-03-09 08:49:33 -0300

    tarefa 4 funcionando

--- Roberto/tarefa4.php
+++ Roberto/tarefa4.php
@@ -1,21 +1,28 @@
 <?php
 
-$ano = $_POST['ano'];
-$mes = $_POST['mes'];
 $dia = $_POST['dia'];
+$mes = $_POST['mes'];
+$ano = $_POST['ano'];
 
-function data($ano,$mes,$dia){
-    if($ano > 1900 and $ano <= 2024 ){
-        echo "True";
-        return;
-    }else{
-        echo "False";
-        return;
+function data($dia,$mes,$ano){
+    if($ano> 1900 and $ano <= 2024){
+        if($mes !=2 and $dia >=1 and $dia <=31){
+            echo "True";
+            return;
+        }if($dia >=1 and $dia <=29){
+            echo "True";
+            return;
+        }else{
+            echo "Esse dia não existe";
+        }
+        }else{
+            echo "False";
+            return;
+        }
     }
-}
 
-echo "$ano , $mes , $dia<br>";
-$teste = data($ano,$mes,$dia);
+echo "$dia/ $mes/ $ano<br>";
+$teste = data($dia,$mes,$ano);
 echo "$teste";
 
-?>  
+?>
